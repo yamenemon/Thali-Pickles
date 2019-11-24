460cc173f057598f6b6b8dfb45576ce4e59bd6a0
yamenemon <ios.emon@gmail.com>
2019-11-25 02:49:54 +0600

    First push for thali and pickles

---
+++ Thali&Pickles/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Thali&Pickles
+//
+//  Created by Emon on 25/11/19.
+//  Copyright © 2019 Emon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let factx = UIScreen.main.bounds.width/320
+let appDefaultColor = 0xFF9300
+let robotoBold = "Roboto-Bold"
+let robotoMedium = "Roboto-Medium"
+let robotoRegular = "Roboto-Medium"
---
+++ Thali&Pickles/MenuVC.swift
@@ -0,0 +1,121 @@
+//
+//  MenuVC.swift
+//  Thali&Pickles
+//
+//  Created by Emon on 24/11/19.
+//  Copyright © 2019 Emon. All rights reserved.
+//
+
+import UIKit
+
+class MenuVC: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+
+    var menuCollectionView: UICollectionView!
+
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureNavigationBar(largeTitleColor: .white, backgoundColor: UIColor(rgb: 0xFF9300), tintColor: UIColor(rgb: appDefaultColor), title: "Menu", preferredLargeTitle: true)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10*factx, left: 10*factx, bottom: 10*factx, right: 10*factx)
+        layout.itemSize = CGSize(width: 145*factx, height: 145*factx)
+        
+        menuCollectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), collectionViewLayout: layout)
+        view.addSubview(menuCollectionView)
+        menuCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        menuCollectionView.delegate = self
+        menuCollectionView.dataSource = self
+        menuCollectionView.backgroundColor = .white
+        menuCollectionView.reloadData()
+    }
+
+    // MARK: - UICollectionViewDataSource protocol
+
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath)
+        
+        for content in cell.contentView.subviews {
+            content.removeFromSuperview()
+        }
+        cell.backgroundColor = UIColor.clear // make cell more visible in our example project
+        
+        let cellName = UILabel(frame: CGRect(x: 0, y: 0, width: cell.contentView.frame.size.width, height: cell.contentView.frame.size.height))
+        cell.contentView.addSubview(cellName)
+        cellName.backgroundColor = UIColor(rgb: appDefaultColor)
+        cellName.textAlignment = .center
+        cellName.textColor = .white
+        cellName.font = UIFont.systemFont(ofSize: 17.0*factx)
+        cellName.text = items[indexPath.row]
+
+        return cell
+    }
+
+    // MARK: - UICollectionViewDelegate protocol
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+
+}
+
+extension UIViewController {
+    func configureNavigationBar(largeTitleColor: UIColor, backgoundColor: UIColor, tintColor: UIColor, title: String, preferredLargeTitle: Bool) {
+        if #available(iOS 13.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: largeTitleColor]
+            navBarAppearance.titleTextAttributes = [.foregroundColor: largeTitleColor]
+            navBarAppearance.backgroundColor = backgoundColor
+
+            navigationController?.navigationBar.standardAppearance = navBarAppearance
+            navigationController?.navigationBar.compactAppearance = navBarAppearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+
+            navigationController?.navigationBar.prefersLargeTitles = preferredLargeTitle
+            navigationController?.navigationBar.isTranslucent = false
+            navigationController?.navigationBar.tintColor = tintColor
+            navigationItem.title = title
+
+        } else {
+            // Fallback on earlier versions
+            navigationController?.navigationBar.barTintColor = backgoundColor
+            navigationController?.navigationBar.tintColor = tintColor
+            navigationController?.navigationBar.isTranslucent = false
+            navigationItem.title = title
+        }
+    }
+    
+}
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}
